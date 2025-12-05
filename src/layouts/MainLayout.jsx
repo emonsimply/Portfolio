9ef8a4c15b690b7367816405a88e05c410b879a2
emonsimply <emonsimply@gmail.com>
2025-12-05 11:12:09 +0600

    initial setup done

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <nav><Navbar></Navbar></nav>
+    </div>
+  );
+};
+
+export default MainLayout;
